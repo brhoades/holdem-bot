8cc092c1b073fa5659ff3ccc17d76c4839fc52ea
Billy Rhoades <billyarhoades@gmail.com>
2015-11-21 00:36:08 -0600

    Rewrote specialkeval to just return the average score.

--- score/specialeval.cpp
+++ score/specialeval.cpp
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+#define NUM_CARDS_IN_DECK 52
+#define MAX_SCORE         7462
+
+int* createDeck(char** argv, int* cards, const int numCards);
+
 int main(int argc, char *argv[])
 {
   if(argc != 6 && argc != 8)
@@ -13,18 +18,71 @@ int main(int argc, char *argv[])
     return 0;
   }
 
+  // of the cards passed, first 3/5 are the table, last 2 are our cards that can't be chosen
+  int* cards = new int[argc-1];
+  for(int i=1; i<argc; i++)
+    cards[i-1] = atoi(argv[i]); 
+
+  int* deck = createDeck(argv, cards, argc-1); 
+  int count=0;
+  unsigned int sum=0;
+
   if(argc == 6)
   {
     FiveEval const eval;
+    
+    // get hand score possibilities for them and sum them
+    for(int i=0; i<NUM_CARDS_IN_DECK-argc+1; i++)
+    {
+      for(int j=0; j<i; j++)
+      {
+        sum += eval.GetRank(cards[0], cards[1], cards[2], deck[i], deck[j]);
+        count++;
+      }
+    }
+  }
+  else
+  {
+    SevenEval const eval;
 
-    cout << eval.GetRank(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[3]), 
-        atoi(argv[5])) << endl;
-    return 0;
+    // get hand score possibilities for them and sum them
+    for(int i=0; i<NUM_CARDS_IN_DECK-argc+1; i++)
+    {
+      for(int j=0; j<i; j++)
+      {
+        sum += eval.GetRank(cards[0], cards[1], cards[2], cards[3], cards[4], 
+            deck[i], deck[j]);
+        count++;
+      }
+    }
   }
 
-  SevenEval const eval;
+  cout << MAX_SCORE - (sum / count) << endl;
 
-  cout << eval.GetRank(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), 
-      atoi(argv[5]), atoi(argv[6]), atoi(argv[7])) << endl;
+  delete deck;
+  delete cards;
   return 0;
+}
+
+inline int* createDeck(char** argv, int* cards, const int numCards)
+{
+  int* deck = new int[NUM_CARDS_IN_DECK-numCards];
+  int decki=0;
+ 
+  for(int i=0; i<NUM_CARDS_IN_DECK; i++)
+  {
+    bool use = true;
+    for(int j=0; j<numCards; j++)
+    {
+      if(i == cards[j])
+      {
+        use=false;
+        break;
+      }
+    }
+    if(use)
+      deck[decki++] = i;
+  }
+
+  return deck;
 }
